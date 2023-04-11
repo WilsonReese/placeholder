d6d771c8e0d619df3158c5dbacd4b00b64f6ee10
WilsonReese <reese@chicagobooth.edu>
2023-04-11 20:19:04 +0000

    changed routes.rb file to be simpler.

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,9 @@
 Rails.application.routes.draw do
-
-
-
-  # Routes for the Reservation resource:
-
-  # CREATE
-  post("/insert_reservation", { :controller => "reservations", :action => "create" })
-          
-  # READ
-  get("/reservations", { :controller => "reservations", :action => "index" })
-  
-  get("/reservations/:path_id", { :controller => "reservations", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_reservation/:path_id", { :controller => "reservations", :action => "update" })
-  
-  # DELETE
-  get("/delete_reservation/:path_id", { :controller => "reservations", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Theater resource:
-
-  # CREATE
-  post("/insert_theater", { :controller => "theaters", :action => "create" })
-          
-  # READ
-  get("/theaters", { :controller => "theaters", :action => "index" })
-  
-  get("/theaters/:path_id", { :controller => "theaters", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_theater/:path_id", { :controller => "theaters", :action => "update" })
-  
-  # DELETE
-  get("/delete_theater/:path_id", { :controller => "theaters", :action => "destroy" })
-
-  #------------------------------
+  root "reservations#index"
 
   devise_for :users
+
+  resources :theaters
+  resources :reservations
+
 end
